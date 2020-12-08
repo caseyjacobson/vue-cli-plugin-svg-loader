22135d9b80136b955844c907689f28f3a082f015
casey6 <casey6@gmail.com>
2020-12-08 11:43:38 -0600

    Add support for IE and older Edge

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = (api, options) => {
       .rule('svg')
       .uses.clear()
       .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
       .options(options.pluginOptions.svgLoader);
